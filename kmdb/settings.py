53a9c11090edb88fb11696cfcff8c9747183d2c4
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-30 09:54:38 -0300

    feat:creatings migrations

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_frameworl.authtoken',
+    'rest_framework.authtoken',
     'users',
     'genres',
     'movies',
