d6cf87f1b34f55b7aa9b2f800e4903597fd6930f
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-30 17:01:58 -0300

    feat:creating review field_choices

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class RecomendationReview(models.TextChoices):
+    MUST_WATCH = ('Must Watch')
+    SHOULD_WATCH = ('Should Watch')
+    AVOID_WATCH = ('Avoid Watch')
+    NO_OPINION = ('No Opinion')
+
 class Review(models.Model):
     stars = models.IntegerField()
     review = models.TextField()
     spoilers = models.BooleanField(default=False)
-    recomendation = models.CharField(max_length=50)
+    recomendation = models.CharField(
+        max_length=50,
+        choices=RecomendationReview.choices,
+        default=RecomendationReview.NO_OPINION
+        )
     movies = models.ForeignKey('movies.Movie', on_delete=models.CASCADE, related_name='reviews')
     users = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviews')
 
