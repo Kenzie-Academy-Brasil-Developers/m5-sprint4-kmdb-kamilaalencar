684b7d2cdd87022f1c56e30fbc9889df58d5389c
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-30 08:50:23 -0300

    feat:creating relationships

--- reviews/models.py
+++ reviews/models.py
@@ -5,4 +5,6 @@ class Review(models.Model):
     review = models.TextField()
     spoilers = models.BooleanField(default=False)
     recomendation = models.CharField(max_length=50)
+    movies = models.ForeignKey('movies.Movie', on_delete=models.CASCADE, related_name='reviews')
+    users = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviews')
 
