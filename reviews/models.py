efadd11115223aa2ca6c2a28471a3e8ef1f3572d
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-30 08:43:01 -0300

    feat:creating model review

---
+++ reviews/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Review(models.Model):
+    stars = models.IntegerField()
+    review = models.TextField()
+    spoilers = models.BooleanField(default=False)
+    recomendation = models.CharField(max_length=50)
+
