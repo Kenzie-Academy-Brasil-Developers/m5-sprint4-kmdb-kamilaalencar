0142c561ac1b28005b78e9c19177385a07776d1a
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-30 13:28:14 -0300

    feat:installing simplejwt.authentication

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,15 @@
+from functools import partial
 from rest_framework.views import APIView, Response, status
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from .permissions import MovieCustomPermission
 from .models import Movie
 from .serializers import MovieSerializer
 
 class MovieView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [MovieCustomPermission]
+
     def post(self, request):
         serializer = MovieSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -15,10 +22,35 @@ class MovieView(APIView):
         return Response(serializer.data, status.HTTP_200_OK)
 
 class MovieViewDetail(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [MovieCustomPermission]
+    
     def get(self, request, movie_id):
         try:
             movie = Movie.objects.get(pk=movie_id)
         except Movie.DoesNotExist:
             return Response({'message':'Movie not found'})
+
         serializer = MovieSerializer(movie)
         return Response(serializer.data, status.HTTP_200_OK)
+
+    def path(self, request, movie_id):
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response({'message':'Movie not found'})
+
+        serializer = MovieSerializer(movie, request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def delete(self, request, movie_id):
+        try:
+            movie = Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return Response({'message':'Movie not found'})
+        
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
